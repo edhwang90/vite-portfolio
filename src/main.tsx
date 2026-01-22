8bc2a731716871fc1c9644179253c31a2f5806c0
Edmond Hwang <42156235+edhwang90@users.noreply.github.com>
2026-01-22 11:00:51 -0500

    posthog analytics
    posthog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { PostHogProvider } from 'posthog-js/react';
 
 import './styles/simple-grid.css';
 import './styles/globals.scss';
@@ -8,12 +9,19 @@ import './styles/layout.scss';
 
 import App from './App.tsx';
 
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30',
+} as const
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="content-container">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </div>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <div className="content-container">
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </div>
+    </PostHogProvider>
   </StrictMode>,
 )
